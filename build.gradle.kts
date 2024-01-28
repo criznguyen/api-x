baa363b2013a6f9a3955e8dc411182b02ec82132
cris <congnguyenit@hotmail.com>
2024-01-28 11:51:03 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
   // https://mvnrepository.com/artifact/com.theokanning.openai-gpt3-java/service
 //  implementation("com.theokanning.openai-gpt3-java:service:0.14.0")
   // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-  implementation("ch.qos.logback:logback-classic:1.4.11")
+  implementation("ch.qos.logback:logback-classic:1.4.14")
 
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
